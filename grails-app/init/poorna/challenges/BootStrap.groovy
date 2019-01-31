811bd1185a4a27f840651591cc8da113e50f27b9
Balaji Sethuraman <balaji@vivasa.in>
2019-01-31 02:59:40 +0000

    Initial Commit

---
+++ grails-app/init/poorna/challenges/BootStrap.groovy
@@ -0,0 +1,9 @@
+package poorna.challenges
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
---
+++ src/main/java/com/challenges/Addition.java
@@ -0,0 +1,12 @@
+package com.challenges;
+
+public class Addition {
+    public int calculation(int x, int y) {
+        int c = x + y;
+        return c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Addition program executed.");
+    }
+}
