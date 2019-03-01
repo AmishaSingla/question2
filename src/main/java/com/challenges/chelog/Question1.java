e07a3b47b36c07ef4f18f659e81639a58c2b070c
Balaji S <balaji@vivasa.in>
2019-03-01 06:49:35 +0000

    chelog exercise added with latest jar

---
+++ src/main/java/com/challenges/chelog/Question1.java
@@ -0,0 +1,25 @@
+package com.challenges.chelog;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.util.*;
+import java.io.*;
+
+/*
+* Run the command from the root directory of this project to validate if this Question is answered correctly
+* './gradlew test -Dtest.single='*chelog/Question1''
+*/
+public class Question1{
+
+    /**
+    * This method returns the downtime of a workspace in rounded hours.
+    * variable intern will have person name as input.using that person name calculate the downtime for that particular person.  
+    * final result of downtime should be in this format 15 . It should not be like 15.45, 16.00.
+    * eg: return 15
+    */
+    public static int downtime(File file, String intern) {
+        return 0; //return the calculated time.
+    }
+}
