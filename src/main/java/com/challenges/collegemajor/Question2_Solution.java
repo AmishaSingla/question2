35614d2fe0feebb792e4cebf99fdc048e0f1c502
aishwarya <aiswarya@vivasa.in>
2019-03-27 11:49:33 +0000

    added collegemajor challenge

---
+++ src/main/java/com/challenges/collegemajor/Question2_Solution.java
@@ -0,0 +1,27 @@
+package com.challenges.collegemajor;
+
+import java.util.*;
+import java.io.*;
+
+import com.challenges.collegemajor.*;
+import com.challenges.tutor.chelog1.Question1_Tutor;
+
+/*
+* 2).Write a program to list all Unique Major_Category(ies) present in the data sheet.       
+* Run the command from the root directory of this project to validate if this Question is answered correctly
+* './gradlew test -Dtest.single='*collegemajor/Question2''
+*/
+
+public class Question2_Solution {
+    /**
+    *Note:While returning the result as a map, in key:value pair always use key as "majorCategory"
+    * A sample return Map look like this:
+    * ["majorCategory":["Engineering",..]
+    * 
+    */
+    public static Map solve(File file){
+      //YOUR CODE GOES HERE
+      return null;
+    }
+
+}
