ed132b72968adf1ec83af69ba7949fd6db1ef2c6
aiswarya <aiswarya@vivasa.in>
2019-04-01 05:19:37 +0000

    added level100 challenge

---
+++ src/main/java/com/challenges/level100/Question3_Solution.java
@@ -0,0 +1,21 @@
+package com.challenges.level100;
+import java.util.*;
+import java.io.*;
+import com.challenges.tutor.level100.*;
+/**
+*3).Look at the sample table below. You will be provided with two lists. One will contain the names of the 
+*   restaurants and the other list will contain its respective cost for 2 people in USD. Store all the provided
+*   data in a Map and return the Map.
+* Run the command from the root directory of this project to validate if this Question is answered correctly
+* './gradlew test -Dtest.single='*level100/Question3''
+*/
+public class Question3_Solution{
+    /**
+    * A sample return Map look like this:
+    * [Taco Bus:10 USD, Genghis Grill:180 USD] 
+    */
+    public static Map solve(List restaurants,List cost){
+
+       return null;
+    }
+}
---
+++ src/main/java/com/challenges/level100/Question4_Solution.java
@@ -0,0 +1,21 @@
+package com.challenges.tutor.level100;
+
+import java.util.*;
+import java.io.*;
+
+import com.challenges.tutor.level100.*;
+/**
+*4).Given two lists with restaurant names and their respective cost for 2 people, store all the data
+*  in a Map and return a smaller map with details of only those restaurants whose cost for 2 is less than USD 200.
+* './gradlew test -Dtest.single='*level100/Question4''
+*/
+public class Question4_Solution{
+    /**
+    * A sample return Map look like this:
+    * [Hot Palayok:100 USD, Denny's:190 USD] 
+    */
+    public static Map solve(List restaurants,List cost){
+
+       return null;
+    }
+}
